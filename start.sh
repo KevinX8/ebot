ca48c70a42e2d2b312c3936700a6ff3df13ca86b
Jklars <pokeofdoom77@gmail.com>
2017-07-06 13:44:01 +0300

    Add a start.sh file

---
+++ start.sh
@@ -0,0 +1,21 @@
+export BOTVERSION=0.1
+
+sudo pip install discord.py
+
+git clone https://github.com/Jklars/ebot.git
+
+cd ebot
+
+export NEWVERSION=$(<version)
+
+python3 checkver.py 0.1 $NEWVERSION
+
+export ISNEW=$(<isnew)
+
+if [ISNEW == True]
+    then
+        cp * ..
+fi
+
+cd ..
+rm ebot
